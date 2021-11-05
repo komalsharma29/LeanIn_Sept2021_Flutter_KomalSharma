dcee7f5e0f180837e98dbbc86feeaf0246620841
Komal Sharma <78088095+komalsharma29@users.noreply.github.com>
2021-11-05 20:54:06 +0530

    Update main.dart

--- Milestone2/main.dart
+++ Milestone2/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+
+void main() => runApp(quizapp());
+
+class quizapp extends StatelessWidget { 
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.grey[700],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[600] ,
+          centerTitle: true,
+          title: const Text(
+            "GET FIT",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,            
+              fontSize: 30),
+            ),
+          ),        
+          body: QuestionText(),
+                        
+          ),
+          );
+  }
+}
+
 
